fa67a67b58416bbe54d7a74f2a4176e896ec4cfa
bapidasdev <das316456@gmail.com>
2023-03-06 21:39:43 +0530

    Initial commit

---
+++ src/components/ProductDesign.jsx
@@ -0,0 +1,44 @@
+
+import React from 'react'
+import { OrbitControls, Stage } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import Shoe from './Shoe';
+import styled from 'styled-components';
+
+const Desc = styled.div`
+  width: 200px;
+  height: 70px;
+  padding: 20px;
+  background-color: white;
+  border-radius: 10px;
+  position: absolute;
+  bottom: 80px;
+  right: 100px;
+
+  @media only screen and (max-width:768px) {
+    top:0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    margin: auto;
+  }
+`
+
+
+const ProductDesign = () => {
+  return (
+    <>
+      <Canvas>
+        <Stage environment="city" intensity={0.6}>
+          <Shoe />
+        </Stage>
+        <OrbitControls enableZoom={false} />
+      </Canvas>
+      <Desc>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum, ad, iure accusamus optio repellat atque vitae</Desc>
+    </>
+
+  )
+}
+
+export default ProductDesign
+
